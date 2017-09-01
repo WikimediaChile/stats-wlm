e8764b67a0535323493047be2f7a1911ec971757
superzerocool <dennis.tobar@gmail.com>
2017-09-01 10:30:00 -0300

    I don't want know the origin country (tor browser)

--- app/helper/country.php
+++ app/helper/country.php
@@ -7,6 +7,7 @@ class country
     public static function getName(string $country) : string
     {
         $languages = explode(",", \F3::get('LANGUAGE'))[0];
+        $languages = explode("-", $languages)[0];
         $result = database::context()->exec('SELECT * from country where code = :country_string and lang = :lang', ['country_string' => $country, 'lang' => $languages]);
         return count($result) === 0 ? ucwords($country) : $result[0]['value'];
     }
